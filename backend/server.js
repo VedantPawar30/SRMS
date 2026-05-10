30f805a4ed1af680febc9f7b46c993f8baae9c69
dh4wall <dhawalshinde14@gmail.com>
2026-05-10 19:46:26 +0530

    Initial commit with app

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const studentRoutes = require('./routes/students');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const path = require("path");
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/students', studentRoutes);
+
+// Health check
+/* app.get('/', (req, res) => {
+  res.json({ message: 'Student Record API is running.' });
+}); */
+
+// Serve React build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
+// Connect to MongoDB and start server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully.');
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1);
+  });
